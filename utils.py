8f526350080ef9701a1955a1cb15fbe58e64fb99
cl-py <ivypool59@gmail.com>
2024-02-09 15:30:44 -0600

    Improved code readability.

--- utils.py
+++ utils.py
@@ -8,11 +8,19 @@ from PIL import Image, ImageGrab
 pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 def retrieve_dps_screenshot():
+    
     screenshot = PIL.ImageGrab.grab(bbox = (1500, 90, 1800, 400))
     screenshot.show()
     return screenshot
-#TESSERACT INSTALLATION REQUIRED
+
 def process_dps_screenshot():
-    dps = pytesseract.image_to_string(retrieve_dps_screenshot())
-    return dps
-print(process_dps_screenshot())
+    
+    string_dps = pytesseract.image_to_string(retrieve_dps_screenshot()) #returns string
+    
+    try:
+        dps = int(string_dps)
+        return dps
+    except:
+        print("Error converting string value to int.")
+        
+print(process_dps_screenshot()) #TESTING ONLY
