f2fdb5119dab631db5cf8ce77026af61035d2a01
cl-py <ivypool59@gmail.com>
2024-02-09 15:15:49 -0600

    Added screenshot to string using pytesseract.

--- utils.py
+++ utils.py
@@ -3,10 +3,12 @@ import pytesseract
 import pyautogui
 import time
 import PIL
-from PIL import Image
+from PIL import Image, ImageGrab
+
+pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 def retrieve_dps_screenshot():
-    screenshot = PIL.ImageGrab.grab(bbox = (1500, 0, 1800, 400))
+    screenshot = PIL.ImageGrab.grab(bbox = (1500, 90, 1800, 400))
     screenshot.show()
     return screenshot
 #TESSERACT INSTALLATION REQUIRED
