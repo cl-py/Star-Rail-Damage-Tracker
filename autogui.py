5210d7aa3e7c6d66b4191d102880dac5947db5c3
cl-py <ivypool59@gmail.com>
2024-02-09 07:39:12 -0600

    Drafted autogui.

---
+++ autogui.py
@@ -0,0 +1,14 @@
+import pyautogui
+import pygetwindow
+
+def find_window():
+    for window in pygetwindow.getAllTitles():
+        if "Honkai: Star Rail" in window:
+            return pygetwindow.getWindowsWithTitle(window)[0]
+        
+def main():
+
+    honkai_window = find_window()
+
+    if honkai_window:
+        print("Found Star Rail Window") #TESTING ONLY
